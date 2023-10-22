d9a307e5c5babc2fb437f4a86c2c3284ebd7ac38
Cowwl <84736016+Cowwl@users.noreply.github.com>
2023-10-23 02:01:34 +0530

    sfczx

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider as StyletronProvider, DebugEngine } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
+import { StyleReset } from "atomize";
 
+const debug =
+  process.env.NODE_ENV === "production" ? void 0 : new DebugEngine();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// 1. Create a client engine instance
+const engine = new Styletron();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StyletronProvider value={engine} debug={debug} debugAfterHydration>
+      <StyleReset />
+      <App />
+    </StyletronProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
