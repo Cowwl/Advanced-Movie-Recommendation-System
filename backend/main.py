db05d32401c6baeb489044c1f42edd8c460a531e
aman-jain13 <ajain13032002@gmail.com>
2023-10-23 02:48:12 +0530

    update

---
+++ backend/main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import torch
+from transformers import BertTokenizer, BertModel, BertForSequenceClassification
+from sklearn.metrics.pairwise import cosine_similarity
+import spacy
+import random
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+import json
+from sentence_transformers import SentenceTransformer
+from transformers import pipeline
+from sklearn.preprocessing import MinMaxScaler
+from spacy import displacy
+from sklearn.preprocessing import StandardScaler
