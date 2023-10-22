f09dc81c2fcb2c8b563103bd37b7f1c5174b4e0e
aman-jain13 <ajain13032002@gmail.com>
2023-10-23 02:27:13 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -55,7 +55,7 @@ const closeDetail = () => {
 return ( 
 	<div className="App"> 
 	<header className="App-header"> 
-		<h1>Movie Mania</h1> 
+		<h1>Movie Streamer</h1> 
 	</header> 
 	<main> 
 		<Search searchInput={searchInput} search={search} /> 
