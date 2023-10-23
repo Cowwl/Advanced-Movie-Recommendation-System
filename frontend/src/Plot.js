44ad0db650dcc5ede00be5ff9cb71a302f9270ba
aman-jain13 <ajain13032002@gmail.com>
2023-10-24 01:34:36 +0530

    aa

--- frontend/src/Plot.js
+++ frontend/src/Plot.js
@@ -1,29 +1,44 @@
 import React, { useEffect, useState } from "react";
 import Search from "./components/Search";
 import Results from "./components/Results";
-import { Link } from "react-router-dom";
 import axios from "axios";
 
 function Plot({ results }) {
   const [movieDetails, setMovieDetails] = useState([]);
-  
+  const [searchInput, setSearchInput] = useState(""); // State to store the user's input
+
+  const handleSearch = () => {
+    // Send a POST request to the backend
+    axios
+      .post("http://localhost:8000/search-plot/", { user_input: searchInput })
+      .then((response) => {
+        // Handle the response data here
+        const movieDetailsData = response.data;
+        setMovieDetails(movieDetailsData);
+      })
+      .catch((error) => {
+        // Handle errors here
+        console.error("Error fetching movie details:", error);
+      });
+  };
+
   useEffect(() => {
-    // Fetch movie details for each imdb_id
+    // Fetch movie details for the initial results
     const fetchMovieDetails = async () => {
       try {
         const movieDetailsPromises = results.map((result) => {
           const imdb_id = result.imdb_id;
-          return axios.get(`https://www.omdbapi.com/?apikey=5426c86e`);
+          return axios.get(`https://www.omdbapi.com/?i=${imdb_id}&apikey=5426c86e`);
         });
-        
+
         const movieDetailsResponses = await Promise.all(movieDetailsPromises);
-        const movieDetailsData = movieDetailsResponses.map(response => response.data);
+        const movieDetailsData = movieDetailsResponses.map((response) => response.data);
         setMovieDetails(movieDetailsData);
       } catch (error) {
         console.error("Error fetching movie details:", error);
       }
     };
-    
+
     fetchMovieDetails();
   }, [results]);
 
@@ -33,9 +48,14 @@ function Plot({ results }) {
         {/* <h1>Movie Streamer</h1> */}
       </header>
       <main>
-        <Search results={results} />
+        <Search
+          results={results}
+          onSearch={(input) => {
+            setSearchInput(input);
+          }}
+          onSearchSubmit={handleSearch} // Handle the search on submit
+        />
         <Results results={results} />
-        {/* Render movie details */}
         {movieDetails.map((movie, index) => (
           <div key={index}>
             <h2>{movie.Title}</h2>
@@ -43,7 +63,6 @@ function Plot({ results }) {
             {/* Add more movie details as needed */}
           </div>
         ))}
-        {/* <Link to="/mood">Go to Mood</Link> */}
       </main>
     </div>
   );
